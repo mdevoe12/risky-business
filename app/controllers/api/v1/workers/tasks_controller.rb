a1211ffb45557858881f6ef0456650f36713f10d
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-23 10:34:43 -0600

    robust test built out for api

---
+++ app/controllers/api/v1/workers/tasks_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Workers::TasksController < ApplicationController
+
+  def create
+    binding.pry
+  end
+
+end
--- spec/requests/api/v1/workers/worker_tasks_spec.rb
+++ spec/requests/api/v1/workers/worker_tasks_spec.rb
@@ -9,13 +9,13 @@ describe "worker tasks api" do
              supervisor_id: "#{supervisor.id}",
              task_description: "replace pipes",
              questions: {
-               1: "replacing a1-a3 pipes in sector v",
-               2: "hazardous chemical spill",
-               3: "severe chemical burns",
-               4: "ensure protective gear correctly worn"
+               q1: "replacing a1-a3 pipes in sector v",
+               q2: "hazardous chemical spill",
+               q3: "severe chemical burns",
+               q4: "ensure protective gear correctly worn"
                         }}
 
-    post "/api/v1/workers/tasks", input
+    post "/api/v1/workers/tasks", params: input
 
     # rack_test_session_wrapper = Capybara.current_session.driver
     # rack_test_session_wrapper.post("/api/v1/workers/tasks",
@@ -29,9 +29,19 @@ describe "worker tasks api" do
     #            4: "ensure protective gear correctly worn"
     #                     }}
 
-    result = JSON.parse(response.body)
-    
-    expect(result.worker_id).to eq(worker.id)
+    # result = JSON.parse(response.body)
 
+    task = Task.last
+
+    expect(response).to be_success
+    expect(Task.count).to eq(1)
+    expect(task.description).to eq("replace pipes")
+    expect(task.worker.id).to eq(worker.id)
+    expect(task.supervisor.id).to eq(supervisor.id)
+    expect(task.points).to eq(nil)
+    expect(task.question_responses.count).to eq(4)
+    expect(task.question_responses.first.body).to eq("replacing a1-a3 pipes in sector v")
+    expect(task.question_responses.last.body).to eq("ensure protective gear correctly worn")
+    
   end
 end
