1ef5ee076c0c1de87d65a0cdc3e142f401e575ba
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-23 21:55:30 -0600

    updated controller, added migration

--- app/controllers/api/v1/workers/tasks_controller.rb
+++ app/controllers/api/v1/workers/tasks_controller.rb
@@ -1,14 +1,18 @@
 class Api::V1::Workers::TasksController < ApplicationController
 
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
+    # binding.pry
+    result = JSON.parse(response.request.body.string, symbolize_names: :true)
 
-    task = Task.create!(description: params[:task_description],
-                 worker_id: params[:worker_id],
-                 supervisor_id: params[:supervisor_id])
-    params[:questions].each do |q, a|
-      task.responses.create!(body: "#{a}", question_num: q.to_i)
-    end
-
+    task = Task.create!(description: result[:Task_Type],
+                          worker_id: result[:worker_id],
+                      supervisor_id: result[:supervisor_id])
+         task.responses.create(body: result[:Exact_Task])
+         task.responses.create(body: result[:Risk_Concerns])
+         task.responses.create(body: result[:description])
+         task.responses.create(body: result[:Mitigation])
   end
 
 end
