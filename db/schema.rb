c84c6d50a7df34010b3090f0384293bf7cb9a37b
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-23 22:19:33 -0600

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923155725) do
+ActiveRecord::Schema.define(version: 20170924034935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 20170923155725) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "worker_risk_score"
+    t.integer "super_risk_score"
+    t.integer "risk_differential"
     t.index ["supervisor_id"], name: "index_tasks_on_supervisor_id"
     t.index ["worker_id"], name: "index_tasks_on_worker_id"
   end
