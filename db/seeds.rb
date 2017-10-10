95e15f09393753a9d5290a13a928cca1a7c6adf4
Ellen Cooper <ellencooper74@gmail.com>
2017-10-09 23:38:20 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,9 +54,7 @@ persons = CSV.parse(file)
       :worker_risk_score => rand(1..7),
       :category_id => rand(1..3),
       :notes => Faker::Hipster.sentence,
-      :follow_up_status => rand(1..2),
-      :created_at => date,
-      :updated_at => date
+      :follow_up_status => rand(1..2)
     )
 
     4.times do |i|
