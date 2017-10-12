bdd232720d5204ae798976772548d35b1afa6c2e
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-12 16:56:11 -0600

    table populating, needs styling

--- app/assets/javascripts/form_results.js
+++ app/assets/javascripts/form_results.js
@@ -72,48 +72,62 @@ $.ajax({
   url: '/api/v1/managers/dashboard',
   success: function(data) {
     flras = data['flras']
-    // debugger
-    Highcharts.chart('flra-scores', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: 0,
-            plotShadow: false
-        },
-        title: {
-            text: 'October<br>FLRA Scores',
-            align: 'center',
-            verticalAlign: 'middle',
-            y: 40
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                dataLabels: {
-                    enabled: true,
-                    distance: -50,
-                    style: {
-                        fontWeight: 'bold',
-                        color: 'white'
-                    }
-                },
-                startAngle: -90,
-                endAngle: 90,
-                center: ['50%', '75%']
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Browser share',
-            innerSize: '50%',
-            data: [
-                ['Satisfactory', flras.satisfactory],
-                ['Needs Improvement', flras.needs_improvement],
-            ]
-        }]
-    });
+    supervisors = data['supervisors']
+    renderFlrasChart(flras)
+    supervisors.forEach(function(supervisor) {
+      // debugger
+    $("#supervisors-header").after(
+      `<tr>
+          <td> ${supervisor.Name} </td>
+          <td> ${supervisor.Reviewed} </td>
+          <td> ${supervisor.Outstanding}</td>
+        </tr> `
+    );
+  })
  }
 })
+
+function renderFlrasChart(flras) {
+  Highcharts.chart('flra-scores', {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: 0,
+          plotShadow: false
+      },
+      title: {
+          text: 'October<br>FLRA Scores',
+          align: 'center',
+          verticalAlign: 'middle',
+          y: 40
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+          pie: {
+              dataLabels: {
+                  enabled: true,
+                  distance: -50,
+                  style: {
+                      fontWeight: 'bold',
+                      color: 'white'
+                  }
+              },
+              startAngle: -90,
+              endAngle: 90,
+              center: ['50%', '75%']
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: 'Browser share',
+          innerSize: '50%',
+          data: [
+              ['Satisfactory', flras.satisfactory],
+              ['Needs Improvement', flras.needs_improvement],
+          ]
+      }]
+  });
+}
 
 });
