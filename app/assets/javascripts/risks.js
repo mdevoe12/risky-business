0c92f377d5bd1d69e120621460abb8ecf4058a8c
Ellen Cooper <ellencooper74@gmail.com>
2017-10-12 22:37:19 -0600

    adds ajax request url

--- app/assets/javascripts/risks.js
+++ app/assets/javascripts/risks.js
@@ -1,14 +1,17 @@
-$(document).ready(){
-  renderCurrentInputs(risks)
-}
+$(document).ready(function(){
+  console.log("hi")
+  renderCurrentInputs("risks")
+})
 
 function renderCurrentInputs(inputType){
+  var categoryId = $('.category_information').data('id')
   $.ajax({
   type: "GET",
-  url:
+  url: "/api/v1/categories/" + categoryId + "/risks"
   })
   .then(function(inputs){
-    $('#current-' + inputType + '-table').append()
+    console.log("yay")
+    // $('#current-' + inputType + '-table').append()
   })
   .catch(function(error){
     console.error(error)
