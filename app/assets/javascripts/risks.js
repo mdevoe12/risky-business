f49263e9c910ba6c86f91c03b091cad309f7442f
Ellen Cooper <ellencooper74@gmail.com>
2017-10-12 23:26:59 -0600

    adds appending of table to ajax success

--- app/assets/javascripts/risks.js
+++ app/assets/javascripts/risks.js
@@ -1,11 +1,22 @@
 function renderCurrentInputs(inputType){
-  var categoryId = $('.category_information').data('id');
-  $.ajax({
-  type: "GET",
-  url: "/api/v1/categories/" + categoryId + "/risks",
-  success: function(inputs){
+    var categoryId = $('.category_information').data('id');
+    $.ajax({
+    type: "GET",
+    url: "/api/v1/categories/" + categoryId + "/risks",
+    success: function(inputs){
+      $("#current-risks-table").append('<h3 class="text-center">' + "Electric" + '</h3>'
+                        + "<table class='table table-bordered'><thead>"
+                        + "<tr><th>Name</th></tr></thead><tbody>"
+                        + generateInputRows(inputs)
+                        + "</tbody></table>")
+    }
+  })
+}
 
-
-  }
-})
+function generateInputRows(inputs){
+  var rows = ""
+  $.each(inputs, function(index, input){
+    rows += "<tr><td>" + input["body"] + "</th></tr>"
+  })
+  return rows
 }
