097bd27ed6b1c706f2843cc8b5a0fbd7acd41fce
Ellen Cooper <ellencooper74@gmail.com>
2017-10-14 14:37:00 -0600

    adds callback to update status after update

--- app/models/flra.rb
+++ app/models/flra.rb
@@ -7,16 +7,21 @@ class Flra < ApplicationRecord
 
   enum follow_up_status: [:not_flagged, :flagged, :resolved]
 
+  before_save :update_followup_status
+
   def self.most_recent
     Flra.all.order("created_at DESC").limit(5)
   end
 
   def calc_diff
     differential = (super_risk_score - worker_risk_score).abs
-    self.update(risk_differential: differential)
+    self.risk_differential = differential
   end
 
-  def update_status
-    self.update(follow_up_status: 1) if (risk_differential > 1 || points < 3)
+  def update_followup_status
+    calc_diff
+    if (self.not_flagged? && (risk_differential > 1 || points < 3))
+      self.follow_up_status = 1
+    end
   end
 end
